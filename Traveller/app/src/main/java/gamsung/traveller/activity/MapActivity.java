a66ee7175712f1125b43aabef4cda2985e215227
Hanbin Ju <robertnori8@gmail.com>
2018-01-14 20:18:39 +0900

    테스트 겸 MapActivity 생성 커밋

---
+++ Traveller/app/src/main/java/gamsung/traveller/activity/MapActivity.java
@@ -0,0 +1,15 @@
+package gamsung.traveller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gamsung.traveller.R;
+
+public class MapActivity extends AppCompatActivity {
+//맵 액티비티입니다(한빈)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
