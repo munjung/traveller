5906d80c8fd7b04755c980e957eea3937e1c43c5
DESKTOP-8BDI4BL\jekan <jekang0123@gmail.com>
2018-01-30 02:33:58 +0900

    문정 커밋 - 4번 화면 추가><(액티비티 이름 바꾸셔도됩니닷)

---
+++ Traveller/app/src/main/java/gamsung/traveller/frag/CalendarFragment.java
@@ -0,0 +1,61 @@
+package gamsung.traveller.frag;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import gamsung.traveller.R;
+
+/**
+ * Created by jekan on 2018-01-30.
+ */
+
+public class CalendarFragment extends Fragment {
+    private CalendarPickerView calendar;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_calendar_picker, container, true);
+
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+        calendar = (CalendarPickerView) rootView.findViewById(R.id.calendar_view);
+        Date today = new Date();
+        calendar.init(today, nextYear.getTime()).inMode(CalendarPickerView.SelectionMode.RANGE);
+        /*calendar.init(today, nextYear.getTime())
+                .withSelectedDate(today)
+                .inMode(CalendarPickerView.SelectionMode.RANGE);*/
+        calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                //for(Date da: calendar.getSelectedDates()){
+                //  Log.d("잘나오냐", da.toString());
+                int month = date.getMonth()+1;
+                int day = date.getDay(); //얘 어떻게 해결할지 고민좀!!!!!
+                int year = date.getYear()+1900;
+                Toast.makeText(getContext(), year+"년"+month+"월"+day+"일",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
+        return rootView;
+    }
+
+}
