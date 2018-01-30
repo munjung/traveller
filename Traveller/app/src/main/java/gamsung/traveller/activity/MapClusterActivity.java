d17f90892aa6ecdceb881df0b66c9db57fb432fc
Hanbin Ju <robertnori8@gmail.com>
2018-01-30 19:39:12 +0900

    한빈 새 모델 PhotoInPlace 추가 커밋

--- Traveller/app/src/main/java/gamsung/traveller/activity/MapClusterActivity.java
+++ Traveller/app/src/main/java/gamsung/traveller/activity/MapClusterActivity.java
@@ -1,15 +1,24 @@
 package gamsung.traveller.activity;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 import gamsung.traveller.R;
 
-public class MapClusterActivity extends AppCompatActivity {
+public class MapClusterActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_cluster);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
