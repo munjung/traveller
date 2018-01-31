8ae7a3b59f17ef7847b6c9d42aff00fe4e6964aa
Jiwon <cjw2609@gmail.com>
2018-01-31 19:35:00 +0900

    지원 커밋

--- Traveller/app/src/main/java/gamsung/traveller/activity/EditLocationActivity.java
+++ Traveller/app/src/main/java/gamsung/traveller/activity/EditLocationActivity.java
@@ -2,14 +2,15 @@ package gamsung.traveller.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ToggleButton;
 
 import gamsung.traveller.R;
+import gamsung.traveller.adapter.CustomPagerAdapter;
 
 /**
  * Created by mj on 2018-01-24.
@@ -20,31 +21,40 @@ public class EditLocationActivity extends AppCompatActivity {
 
     ToggleButton eatBtn, doBtn, takeBtn, someBtn;
     EditText memoEdit,editLocation;
-    ImageView memoImage;
+    ViewPager pager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_location);
 
+        pager= (ViewPager)findViewById(R.id.pager);
+
         eatBtn= (ToggleButton)findViewById(R.id.eatToggleBtn);
         doBtn = (ToggleButton)findViewById(R.id.doToggleBtn);
         takeBtn = (ToggleButton)findViewById(R.id.takeToggleBtn);
         someBtn = (ToggleButton)findViewById(R.id.somethingToggleBtn);
-        memoEdit = (EditText)findViewById(R.id.memoEdit);
-        memoImage = (ImageView)findViewById(R.id.memoImage);
+ //       memoEdit = (EditText)findViewById(R.id.memoEdit);
+//        memoImage = (ImageView)findViewById(R.id.memoImage);
         editLocation = (EditText)findViewById(R.id.editLocation);
 
         eatBtn.setOnCheckedChangeListener(addMemoListener);
         doBtn.setOnCheckedChangeListener(addMemoListener);
         takeBtn.setOnCheckedChangeListener(addMemoListener);
         someBtn.setOnCheckedChangeListener(addMemoListener);
+
+        CustomPagerAdapter adapter= new CustomPagerAdapter(getLayoutInflater());
+        pager.setAdapter(adapter);
+
     }
+
+
     public void setVisibleMemo(){
         memoEdit.setText(null);
         memoEdit.setVisibility(View.VISIBLE);
-        memoImage.setVisibility(View.VISIBLE);
+//        memoImage.setVisibility(View.VISIBLE);
     }
+
     ToggleButton.OnCheckedChangeListener addMemoListener  = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -78,10 +88,7 @@ public class EditLocationActivity extends AppCompatActivity {
                         setVisibleMemo();
                         break;
                 }
-
             }
         }
     };
-
-
 }
