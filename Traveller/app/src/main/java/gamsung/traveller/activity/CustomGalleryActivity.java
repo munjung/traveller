e14608a82b8f57efeaf22ba66c09b02b1e9e24f7
DESKTOP-8BDI4BL\jekan <jekang0123@gmail.com>
2018-02-01 03:32:26 +0900

    문정 커밋 - 22번 화면(ImageMultiplePickerActivity) 추가 / 다중선택한 사진들의 경로처리는 아직 못함 ㅠㅠ

---
+++ Traveller/app/src/main/java/gamsung/traveller/activity/CustomGalleryActivity.java
@@ -0,0 +1,187 @@
+package gamsung.traveller.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import gamsung.traveller.R;
+import gamsung.traveller.adapter.GridView_Adapter;
+
+/**
+ * Created by jekan on 2018-02-01.
+ */
+
+//갤러리에서 이미지 다중선택 부분(22번 화면)
+public class CustomGalleryActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+    private static Button selectImages;
+    private static GridView galleryImagesGridView;
+    private static ArrayList<String> galleryImageUrls;
+    private static GridView_Adapter imagesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_gallery);
+
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            initViews();
+            setListeners();
+            fetchGalleryImages();
+            setUpGridView();
+        }
+    }
+
+    //Init all views
+    private void initViews() {
+        selectImages = (Button) findViewById(R.id.selectImagesBtn);
+        galleryImagesGridView = (GridView) findViewById(R.id.galleryImagesGridView);
+
+    }
+
+    //fetch all images from gallery
+    private void fetchGalleryImages() {
+        final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};//get all columns of type images
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;//order data by date
+        Cursor imagecursor = managedQuery(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+                null, orderBy + " DESC");//get all data in Cursor by sorting in DESC order
+
+        galleryImageUrls = new ArrayList<String>();//Init array
+
+
+        //Loop to cursor count
+        for (int i = 0; i < imagecursor.getCount(); i++) {
+            imagecursor.moveToPosition(i);
+            int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);//get column index
+            galleryImageUrls.add(imagecursor.getString(dataColumnIndex));//get Image from column index
+            System.out.println("Array path" + galleryImageUrls.get(i));
+        }
+    }
+
+    //Set Up GridView method
+    private void setUpGridView() {
+        imagesAdapter = new GridView_Adapter(CustomGalleryActivity.this, galleryImageUrls, true);
+        galleryImagesGridView.setAdapter(imagesAdapter);
+    }
+
+    //Set Listeners method
+    private void setListeners() {
+        selectImages.setOnClickListener(this);
+    }
+
+
+    //Show hide select button if images are selected or deselected
+    public void showSelectButton() {
+        ArrayList<String> selectedItems = imagesAdapter.getCheckedItems();
+        if (selectedItems.size() > 0) {
+            selectImages.setText(selectedItems.size() + " - Images Selected");
+            selectImages.setVisibility(View.VISIBLE);
+        } else
+            selectImages.setVisibility(View.GONE);
+
+    }
+
+    //일정 수준의 버전 확인
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.selectImagesBtn:
+
+                //When button is clicked then fill array with selected images
+                ArrayList<String> selectedItems = imagesAdapter.getCheckedItems();
+
+                //Send back result to MainActivity with selected images
+                Intent intent = new Intent();
+                intent.putExtra(ImageMultiplePickerActivity.CustomGalleryIntentKey, selectedItems.toString());//Convert Array into string to pass data
+                setResult(RESULT_OK, intent);//Set result OK
+                finish();//finish activity
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                    Toast.makeText(this, "뭘까이건",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "GET_ACCOUNTS Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
+    }
+}
---
+++ Traveller/app/src/main/java/gamsung/traveller/adapter/InitImage.java
@@ -0,0 +1,38 @@
+package gamsung.traveller.adapter;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * Created by jekan on 2018-02-01.
+ */
+
+public class InitImage  extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initImageLoader(getApplicationContext());
+    }
+
+
+    //Initiate Image Loader Configuration
+    public static void initImageLoader(Context context) {
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(
+                context);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        config.writeDebugLogs(); // Remove for release app
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+
+    }
+}
