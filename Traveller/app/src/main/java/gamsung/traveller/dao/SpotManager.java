20c7ede0026c498df1a69e4237576bf6a0afea04
Yoonroh <shdbs111@gmail.com>
2018-02-21 18:35:01 +0900

    spot 정렬 >오름차순

--- Traveller/app/src/main/java/gamsung/traveller/dao/SpotManager.java
+++ Traveller/app/src/main/java/gamsung/traveller/dao/SpotManager.java
@@ -96,6 +96,7 @@ public class SpotManager {
 
         StringBuffer sb = new StringBuffer();
         sb.append("SELECT * FROM " + TABLE_NAME);
+        sb.append(" ORDER BY " + TableManager.SpotTable.column_id + " ASC");
 
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor c = db.rawQuery(sb.toString(), null);
