dc0257401bc8e9ff0d09a79d61a94a5a07ffb581
Hanbin Ju <robertnori8@gmail.com>
2018-03-01 16:50:49 +0900

    위젯 로직 완성 커밋

--- Traveller/app/src/main/java/gamsung/traveller/activity/HereAppWidget.java
+++ Traveller/app/src/main/java/gamsung/traveller/activity/HereAppWidget.java
@@ -3,9 +3,18 @@ package gamsung.traveller.activity;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.ListView;
 import android.widget.RemoteViews;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import gamsung.traveller.R;
 import gamsung.traveller.dao.DataManager;
@@ -20,19 +29,34 @@ public class HereAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        String mission = "미션 정보 없음";
+        String place = "위치 정보 없음";
+        String nextmisson = "다음 장소 정보 없음";
         DataManager dataManager = DataManager.getInstance(context);
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.here_app_widget);
 
-        HashMap<Integer,Spot> spotlist = dataManager.getSpotListWithRouteId(1);
+        Spot curSpot = new Spot();
+        HashMap<Integer,Spot> spotlist = new HashMap<>();
+        spotlist = dataManager.getSpotListWithRouteId(1);
+
+        Iterator it = sortByValue(spotlist).iterator();
         HashMap<Integer,SearchPlace> placelist = dataManager.getSearchPlaceList();
-        views.setTextViewText(R.id.tvTodowidget,spotlist.get(1).getMission());
 
-        int searchid = spotlist.get(1).getSearch_id();
-        views.setTextViewText(R.id.tvTodolocal,placelist.get(searchid).getPlace_address());
-        views.setTextViewText(R.id.tvTodonext,spotlist.get(1).getMission());
-
+        while(it.hasNext()){
+            int temp = (int)it.next();
+            if(dataManager.getPhotoListWithSpot(temp).size()==0) {
+                mission = spotlist.get(temp).getMission();
+                place = placelist.get(spotlist.get(temp).getSearch_id()).getPlace_address();
+                int newtemp = (int)it.next();
+                nextmisson = spotlist.get(newtemp).getMission();
+                break;
+            }
+        }
+        views.setTextViewText(R.id.tvTodowidget,mission);
+        views.setTextViewText(R.id.tvTodolocal,place);
+        views.setTextViewText(R.id.tvTodonext,nextmisson);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -55,5 +79,24 @@ public class HereAppWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    public static List sortByValue(final Map<Integer,Spot> map){
+        List<Integer> list =new ArrayList<>();
+        list.addAll(map.keySet());
+
+        Collections.sort(list, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Object v1 = map.get(o1).getIndex_id();
+                Object v2 = map.get(o2).getIndex_id();
+
+                return ((Comparable)v2).compareTo(v1);
+            }
+        });
+        Collections.reverse(list);
+        return list;
+    }
 }
+
+
 
